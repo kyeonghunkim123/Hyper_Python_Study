f9d82f92885d4475d59e82076af41afffa9fc6e6
TJE_khkim <kyeonghunkim123@gmail.com>
2023-04-28 16:05:14 +0900

    230428 오후4시 로그인/로그아웃 기능 커밋

--- pybo/models.py
+++ pybo/models.py
@@ -7,7 +7,6 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
-
     def __str__(self):
         return self.subject
 
--- pybo/views.py
+++ pybo/views.py
@@ -1,25 +1,63 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Question, Answer
+from django.utils import timezone
 
+from .models import Question, Answer
+from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
+# ------- 제네릭뷰 시작 -------
+# class IndexView(generic.ListView):
+#     def get_queryset(self):
+#         return Question.objects.order_by('-create_date')
+#
+#
+# class DetailView(generic.DetailView):
+#     model = Question
+# ------- 제네릭뷰 끝 -------
+
+
 
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list' : question_list}
-    return render(request,'pybo/question_list.html', context)
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
+    return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    # question = get_object_or_404(Question, pk=question_id)
+    question = Question.objects.get(id=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    answer.save()
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
-
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():  # 폼이 유효하다면
+            question = form.save(commit=False)  # 임시 저장하여 question 객체를 리턴받는다.
+            question.create_date = timezone.now()  # 실제 저장을 위해 작성일시를 설정한다.
+            question.save()  # 데이터를 실제로 저장한다.
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
